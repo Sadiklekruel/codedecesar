d11daad89a0edd549d4354cdc69489c03d6a21c6
Tibau <xiliar1888@hotmail.fr>
2017-02-13 17:06:22 +0100

    Fonctionnel pour majuscule, Z renvoie au début

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var decalage = 1;
+var decalage = 3;
 var messageElement = document.getElementById('textarea');
 var resultat = document.getElementById('resultat');
 var bouton = document.getElementById('bouton');
 
 
-
 bouton.addEventListener("click", function() {
     var messageValue = messageElement.value;
-    for (var i = 0; i < alphabet.length; i++) {
-        alphabet[i]
+    for (var i = 0; i < messageValue.length; i++) {
+        var indiceLettre = alphabet.indexOf(messageValue[i])
+        var indiceDecale = indiceLettre + decalage;
+        if (indiceDecale >= alphabet.length) {
+            indiceDecale = indiceDecale - 26;
+        }
+        var lettreDecale = alphabet[indiceDecale];
+        resultat.value += lettreDecale;
     }
-
-
-
-
 });
